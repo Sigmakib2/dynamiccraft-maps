7732833e499a3bca1942283b9ca4fc18aa9bb822
DB_Retro <125267441+DBRetro21@users.noreply.github.com>
2025-05-04 19:09:04 +0100

    Forest SMP map Uploaded

---
+++ maps/forestsmp/overviewerConfig.js
@@ -0,0 +1,157 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "Forest SMP"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1746164065",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        },
+        "attribution": null
+    },
+    "tilesets": [
+        {
+            "name": "S1 Day UR",
+            "zoomLevels": 6,
+            "defaultZoom": 1,
+            "maxZoom": 6,
+            "path": "DB_Retro UR",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Forest SMP",
+            "last_rendertime": 1746025439,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                13,
+                176,
+                -28
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                13,
+                176,
+                -28
+            ],
+            "north_direction": 1
+        },
+        {
+            "name": "S1 Day UL",
+            "zoomLevels": 6,
+            "defaultZoom": 1,
+            "maxZoom": 6,
+            "path": "DB_Retro UL",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Forest SMP",
+            "last_rendertime": 1746025439,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                13,
+                176,
+                -28
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                13,
+                176,
+                -28
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "S1 Day LR",
+            "zoomLevels": 6,
+            "defaultZoom": 1,
+            "maxZoom": 6,
+            "path": "DB_Retro LR",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Forest SMP",
+            "last_rendertime": 1746025439,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                13,
+                176,
+                -28
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                13,
+                176,
+                -28
+            ],
+            "north_direction": 2
+        },
+        {
+            "name": "S1 Day LL",
+            "zoomLevels": 6,
+            "defaultZoom": 1,
+            "maxZoom": 6,
+            "path": "DB_Retro LL",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Forest SMP",
+            "last_rendertime": 1746025439,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                13,
+                176,
+                -28
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                13,
+                176,
+                -28
+            ],
+            "north_direction": 3
+        }
+    ]
+};
