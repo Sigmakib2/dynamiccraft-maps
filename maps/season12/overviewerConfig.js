d3490f99f612a8933acb98c204b2adbbbb7620a6
DB_Retro <125267441+DBRetro21@users.noreply.github.com>
2025-05-05 23:04:14 +0100

    Season 11 and 12 Uploaded

---
+++ maps/season12/overviewerConfig.js
@@ -0,0 +1,101 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "Spawn"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1746477649",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        },
+        "attribution": null
+    },
+    "tilesets": [
+        {
+            "name": "S11",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "DB_Retro",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Spawn",
+            "last_rendertime": 1746476807,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                18,
+                144,
+                37
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                18,
+                144,
+                37
+            ],
+            "north_direction": 1
+        },
+        {
+            "name": "S11 Night",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "DB_Retro_N",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Spawn",
+            "last_rendertime": 1746476807,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                18,
+                144,
+                37
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                18,
+                144,
+                37
+            ],
+            "north_direction": 1
+        }
+    ]
+};
